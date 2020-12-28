29b39ed49c9c9492c743b6d29ec16fb3cc5a2d84
Promise <2738023398@qq.com>
2020-12-28 16:20:39 +0800

    v2 加载地图文件功能

---
+++ src/main/java/pushBox/LoadMap.java
@@ -0,0 +1,48 @@
+package pushBox;
+
+import java.io.*;
+
+public class LoadMap {
+    private int[][] map = new int[20][20];
+
+    public int[][] getMap() {
+        return map;
+    }
+
+    public LoadMap(int level) {
+        File file=new File("maps/"+level+".map");
+        BufferedReader bufferedReader = null;
+        FileReader fileReader = null;
+
+        try {
+            fileReader = new FileReader(file);
+            bufferedReader = new BufferedReader(fileReader);
+
+            for (int i=0; i<20; i++){
+                String line = bufferedReader.readLine();
+                byte[] bytes = line.getBytes();
+
+                for (int j=0; j<20; j++) {
+                    map[i][j] = bytes[j] - 48;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (fileReader != null) {
+                    fileReader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
